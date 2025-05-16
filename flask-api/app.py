8e746f6a889d23ddf49cdf17c5b957dd311a7be4
Goudet Ethan <ethangoudet@gmail.com>
2025-05-16 16:01:12 +0200

    commentaire et réponse

--- flask-api/app.py
+++ flask-api/app.py
@@ -7,6 +7,8 @@ from config import Config
 from models import db
 from routes.auth import auth_bp, bcrypt
 from routes.posts import posts_bp
+from routes.replies import replies_api
+from routes.comments import comments_api
 from services.file_upload import init_cloudinary
 from routes.categories import categories_bp
 
@@ -23,6 +25,8 @@ def create_app(config_class=Config):
     app.register_blueprint(auth_bp)
     app.register_blueprint(posts_bp)
     app.register_blueprint(categories_bp)
+    app.register_blueprint(replies_api)
+    app.register_blueprint(comments_api)
     
     with app.app_context():
         from models.post_media import PostMedia
---
+++ flask-api/models/replie.py
@@ -0,0 +1,10 @@
+from models import db
+from datetime import datetime
+
+class Replie(db.Model):
+    __tablename__ = 'replies'
+    id = db.Column(db.Integer, primary_key=True)
+    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
