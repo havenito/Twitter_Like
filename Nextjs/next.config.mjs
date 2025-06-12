d14d58204d836ace0aba7a658b0873b5b055f5d4
Dr4c0niX <149779025+Dr4c0niX@users.noreply.github.com>
2025-06-12 14:42:42 +0200

    support des pfp pinterest

--- Nextjs/next.config.mjs
+++ Nextjs/next.config.mjs
@@ -18,6 +18,11 @@ const nextConfig = {
         hostname: 'avatars.githubusercontent.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        pathname: '/**',
+      },
     ],
   },
 };
