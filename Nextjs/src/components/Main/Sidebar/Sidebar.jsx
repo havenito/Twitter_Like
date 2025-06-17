439efab4299d270d834f6609cea7e72483af50c0
Dr4c0niX <149779025+Dr4c0niX@users.noreply.github.com>
2025-06-17 21:50:51 +0200

    change /mesage to messages

--- Nextjs/src/components/Main/Sidebar/Sidebar.jsx
+++ Nextjs/src/components/Main/Sidebar/Sidebar.jsx
@@ -13,7 +13,6 @@ const Sidebar = ({ onCreatePost }) => {
   const menuItems = [
     { icon: faUser, label: "Profil", href: session?.user?.pseudo ? `/${session.user.pseudo}` : "#" },
     { icon: faSearch, label: "Recherche", href: "/search" },
-    { icon: faHistory, label: "Historique", href: "/history" },
     { icon: faStar, label: "Favoris", href: session?.user?.pseudo ? `/${session.user.pseudo}/favorites` : "#" },
     { icon: faBell, label: "Notifications", href: "/notifications" },
     { icon: faEnvelope, label: "Messages", href: "/messages" },
